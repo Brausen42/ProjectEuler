3fc7a1fd8128c963e4d0707e16a75d2dedfd193a
Alex Brausen <alex.brausen@tr.com>
2016-07-28 11:25:51 -0500

    updating problem 24

--- problem24.clj
+++ problem24.clj
@@ -1,23 +1,42 @@
 (defn remove_element
   [n lis]
-  (into [] (concat (subvec lis 0 n) (subvec lis (inc n)))))
+  (loop [front []
+         [h & tail] lis]
+    (if (= h n)
+      (concat (reverse front) tail)
+      (recur (cons h front) tail))))
+
+(defn insert_into
+  [n i lis]
+  (loop [front []
+         cur 0
+         [h & tail] lis]
+    (if (= cur i)
+      (if (nil? h)
+        (reverse (cons n front))
+        (concat (reverse (cons h (cons n front))) tail))
+      (recur (cons h front) (inc cur) tail))))
 
 (defn map_to_front
   [h lis]
-  (into [] (map #(into [] (concat [h] %)) lis)))
+  (map #(cons h %) lis))
 
 (defn permute
   [lis]
   (if (<= (count lis) 1)
     [lis]
-    (loop [[h & tail] (range (count lis))
+    (loop [[h & tail] lis
            perms []]
       (if (empty? tail)
-        (concat perms (map_to_front (get lis h) (permute (remove_element h lis))))
-        (recur tail (concat perms (map_to_front (get lis h) (permute (remove_element h lis)))))))))
+        (concat perms (map_to_front h (permute (remove_element h lis))))
+        (recur tail (concat perms (map_to_front h (permute (remove_element h lis)))))))))
 
-(remove_element 0 [1 2 3 4 5])
+
+(remove_element 2 '(1 2 3))
 
 (map_to_front 0 [[1 2 3] [4 5 6]])
 
-(count (permute [0 1 2 3 4 5 6 7 8 9]))
+(insert_into 0 0 '(1 2 3))
+
+(permute [0 1 2])
+
