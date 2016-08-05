e3bc0bbf8c0d19c04f413bcbe823f1e56c8784b0
Alex Brausen <alex.brausen@tr.com>
2016-08-05 15:22:12 -0500

    Completed problems 24 and 25

--- problem24.clj
+++ problem24.clj
@@ -31,6 +31,19 @@
         (concat perms (map_to_front h (permute (remove_element h lis))))
         (recur tail (concat perms (map_to_front h (permute (remove_element h lis)))))))))
 
+(defn other_permute
+  [lis]
+  (loop [[h & tail] lis
+         length 0
+         perms [[]]]
+    (if (nil? h)
+      perms
+      (recur tail (inc length) (loop [new_perms []
+                                      index 0]
+                                 (if (> index length)
+                                   new_perms
+                                   (recur (concat new_perms (map #(into [] (insert_into h index %)) perms)) (inc index))))))))
+
 
 (remove_element 2 '(1 2 3))
 
@@ -38,5 +51,33 @@
 
 (insert_into 0 0 '(1 2 3))
 
-(permute [0 1 2])
+(permute [0 1 2 3])
 
+(get [1 2 3] 0)
+
+(time (other_permute [0 1 2 3 4 5 6 7 8 9]))
+
+; Not mine but I couldn't get both efficiency and lexicographic properties and also this approach is neat
+(let [! (fn [n] (reduce * (range 1 (inc n))))]
+  (loop [available-digits (range 10)
+         num 1000000
+         current-digit 0
+         init 9
+         result []]
+    (let [f (! init)]
+      (cond
+        (= 0 init)
+        (apply str (concat result available-digits))
+
+        (< f num) (recur available-digits
+                         (- num f)
+                         (inc current-digit)
+                         init
+                         result)
+
+        :else (recur (concat (take current-digit available-digits)
+                             (drop (inc current-digit) available-digits))
+                     num
+                     0
+                     (dec init)
+                     (conj result (nth available-digits current-digit)))))))
